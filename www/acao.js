3a68a32f0be4d5246699a0bfb2e4cb3990ea752a
anabeatrizzz <anaaugusto2012@bol.com.br>
2020-03-06 00:50:49 +0900

    Fazendo calculadora funcionar do jeito que quero

--- www/acao.js
+++ www/acao.js
@@ -1,33 +1,18 @@
 $(function(){
-  /*var soma = 0;
-  var subtracao = 0
-  var divisao = 0;
-  var multiplicacao = 0;
-  var textoOp = "";*/
   var texto = "";
   var p = 0;
-  var op = 0;
+  var op = "";
   var s = 0;
   $(".numero, .operador").click(function(){
-    $("#caixaTexto").attr("value", $(this).text())
     texto += $(this).text()
     $("#caixaTexto").attr("value", texto)
-
-    var p = Number(texto.substring(0, 1))
-    var op = Number(texto.substring(1, 2))
-    var s = Number(texto.substring(2, 3))
-    $("#caixaTexto").attr("value", eval(`${p} ${op} ${s}`))
-    //alert(texto)
-    /*soma += Number($(this).text())
-    alert(soma)*/
   })
-
-  /*$("#igual").click(function(){
-    $("#caixaTexto").attr("value", eval())
+  $("#igual").click(function(){
+    var caixaTexto = $("#caixaTexto").attr("value")
+    $("#caixaTexto").attr("value", eval(caixaTexto))
   })
-  $(".operador").click(function(){
-    $("#caixaTexto").attr("value", $(this).text())
-    textoOp += $(this).text()
-    $("#caixaTexto").attr("value", textoOp)
-  })*/
+  $("#apagar").click(function(){
+    $("#caixaTexto").attr("value", "")
+    texto = ""
+  })
 })
